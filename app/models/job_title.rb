f68fe5799c541ccb075fb675af7dde7d58d24c9d
dcuddhy <dcuddhy@gmail.com>
2014-11-18 12:09:47 -0700

    consider it done.

--- app/models/job_title.rb
+++ app/models/job_title.rb
@@ -1,3 +1,8 @@
 class JobTitle < ActiveRecord::Base
 
+  has_many :employments
+  has_many :organizations, through: :employments
+  has_many :people, through: :employments
+  has_many :locations, through: :employments
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,8 @@
 class Location < ActiveRecord::Base
 
+  has_many :employments
+  has_many :organizations, through: :employments
+  has_many :job_titles, through: :employments
+  has_many :people, through: :employments
+
 end
